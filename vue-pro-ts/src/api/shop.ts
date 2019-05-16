ab694fa5ed0a3684dcf89273c87448f1221d9b3c
antbrothers <jianxi_lin@163.com>
2019-05-16 10:27:50 +0800

    添加新的模块

--- vue-pro-ts/src/api/shop.ts
+++ vue-pro-ts/src/api/shop.ts
@@ -1,1 +1,21 @@
-import { CartProduct } from "../store";
+import { CartProduct } from "../store"
+const initProducts = [
+  { id: 1, title: 'iPad 4 Mini', price: 500.01, inventory: 2 },
+  { id: 2, title: 'H&M T-Shirt White', price: 10.99, inventory: 10 },
+  { id: 3, title: 'Charli XCX - Sucker CD', price: 19.99, inventory: 5 },
+]
+
+export default {
+  getProducts(cb: (products: any[]) => void) {
+    setTimeout(() => {
+      cb(initProducts)
+    }, 100)
+  },
+  buyProducts(products: CartProduct[], cb: () => void, errorCb: () => void) {
+    setTimeout(() => {
+      Math.random() > 0.5 || navigator.userAgent.indexOf('PhantomJS') > -1
+        ? cb()
+        : errorCb()
+    }, 100);
+  }
+}
--- vue-pro-ts/src/store/modules/products.ts
+++ vue-pro-ts/src/store/modules/products.ts
@@ -21,6 +21,30 @@ const getters = {
 // actioins 
 const actions= {
   getAllProducts(context: ActionContextBasic) {
-    
+    shop.getProducts((products: Product[]) => {
+      const payload: ProductsPayload = {
+        products
+      }
+      context.commit(types.RECEIVE_PRODUCTS, payload)
+    })
   }
+}
+
+// mutations 
+const mutations = {
+  [types.RECEIVE_PRODUCTS] (state: State, payload: ProductsPayload) {
+    state.all = payload.products
+  },
+  [types.ADD_TO_CART] (state: State, payload: AddToCartPayload) {
+    const product = state.all.find((p) => p.id === payload.id)
+    if (product) {
+      product.inventory--
+    }
+  }
+}
+export default {
+  state: initState,
+  getters,
+  actions,
+  mutations
 }
